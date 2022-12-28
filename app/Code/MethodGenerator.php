2149b3d0995752d86bd1e1d5d5fba41b828389d1
Daniel Assani <devasddaniel@gmail.com>
2022-12-28 15:39:17 +0200

    method generator created

---
+++ app/Code/MethodGenerator.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Code; 
+
+use Nette\PhpGenerator\Method;
+
+class MethodGenerator extends CodeGenerator{
+     
+   public function __construct(){
+    
+   }
+         
+
+
+
+
+}
+
+
+?>
